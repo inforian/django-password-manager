339330dc9d5b48f8ae501216ba694df85700719a
inforian <ndhiman08@gmail.com>
2019-10-15 15:30:07 +0530

    Update: package version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 setup(
     name='django-password-manager',
-    version='0.1.1',
+    version='0.1.2',
     description='Enable Password Force Reset & Maintain Password History',
     author='Neeraj Dhiman',
     author_email='ndhiman08@gmail.com',
