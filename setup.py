f43a8d0b206e1fc130584702467f9fe4dc217741
Neeraj Dhiman <neeraj@example.com>
2023-10-27 15:32:56 +0530

    Update: django pkg

--- setup.py
+++ setup.py
@@ -4,17 +4,17 @@ from setuptools import find_packages
 
 setup(
     name='django-password-manager',
-    version='0.1.2',
+    version='0.1.3',
     description='Enable Password Force Reset & Maintain Password History',
     author='Neeraj Dhiman',
     author_email='ndhiman08@gmail.com',
     license='GPL',
     url='https://github.com/inforian/django-password-manager',
-    download_url='https://github.com/inforian/django-password-manager/archive/v0.1.1.tar.gz',
+    download_url='https://github.com/inforian/django-password-manager/archive/v0.1.3.tar.gz',
     keywords=['password', 'django-password', 'django-password-manager', 'django-password-manage'],
     classifiers=[],
     packages=find_packages(),
     install_requires=[
-        'Django>=1.9',
+        'Django>=4.0',
     ]
 )
